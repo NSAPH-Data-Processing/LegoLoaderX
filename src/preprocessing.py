783fa1a0fff582ab875a5706232d4029beff2666
James Celi Kitch <jckitch31@gmail.com>
2025-03-28 11:42:42 -0400

    speed up duckdb query Fixes #10

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,5 +1,7 @@
 import duckdb
 import hydra
+import os
+import pandas as pd
 
 
 @hydra.main(config_path="../conf", config_name="conf", version_base=None)
@@ -8,17 +10,33 @@ def main(cfg):
     month,day = None, None
     timestr = str(cfg.timestr)
     year = timestr[:4]
-    if cfg.temporal_res == "monthly" or cfg.temporal_res == "daily":
+    if cfg.temporal_res == "yearly":
+        time_col = "year"
+        time_query = f"""SELECT {cfg.spatial_res}, '{year}' AS year"""
+        match_query = f"""ON (i.{cfg.spatial_res} = d.{cfg.spatial_res} AND i.year = d.year)"""
+    elif cfg.temporal_res == "monthly":
         month = timestr[4:6]
-    if cfg.temporal_res == "daily":
+        time_col = "year, month"
+        time_query = f"""SELECT {cfg.spatial_res}, '{year}', '{month}' AS year, month"""
+        match_query = f"""ON (i.{cfg.spatial_res} = d.{cfg.spatial_res} AND 
+                              i.year = d.year AND
+                              i.month = d.month)"""
+    elif cfg.temporal_res == "daily":
+        month = timestr[4:6]
         day = timestr[6:8]
+        time_col = "date"
+        time_query = f"""SELECT {cfg.spatial_res}, DATE '{year}-{month}-{day}' AS date"""
+        match_query = f"""ON (i.{cfg.spatial_res} = d.{cfg.spatial_res} AND i.date = d.date)"""
 
     # getting input filepath
     cfg_vg = cfg.var_group
     input_fname = f"{cfg.input_dir}/{cfg_vg.lego_dir}/{cfg_vg.lego_nm}/{cfg.spatial_res}_{cfg.temporal_res}/{cfg_vg.lego_nm}__{cfg.spatial_res}_{cfg.temporal_res}__{year}.parquet"
 
     # setting output filepath
-    output_fname = f"{cfg.output_dir}/{cfg.vg_name}__{cfg.var}__{year}"
+    out_dir = f"{cfg.output_dir}/{cfg.vg_name}/{cfg.var}/"
+    os.makedirs(out_dir, exist_ok=True)
+
+    output_fname = f"{out_dir}/{cfg.var}__{year}"
     if month:
         output_fname += str(month).zfill(2)
     if day:
@@ -28,20 +46,13 @@ def main(cfg):
     # getting unique id list
     uniq_path = f"{cfg.input_dir}/{cfg.uniqid_dir}/{cfg.uniqid_nm}/{cfg.spatial_res}_yearly/{cfg.uniqid_nm}__{cfg.spatial_res}_yearly__{year}.parquet"
 
-    # Create index table
     duckdb.execute(f"""
-        CREATE TABLE index AS SELECT * FROM read_parquet('{uniq_path}') ORDER BY {cfg.spatial_res}
+        CREATE TABLE index AS 
+        {time_query}
+        FROM read_parquet('{uniq_path}') 
+        WHERE continental_us = TRUE
+        ORDER BY {cfg.spatial_res}
     """)
-
-    # Construct filtering condition
-    if day:
-        filter_condition = f"WHERE date = '{year}-{month}-{day}'"
-    elif month:
-        filter_condition = f"WHERE year = '{year}' AND month = '{month}'"
-    elif year:
-        filter_condition = f"WHERE year = '{year}'"
-    else:
-        filter_condition = ""
 
     # Optimized query: Apply filtering *before* joining
     query = f"""
@@ -49,10 +60,9 @@ def main(cfg):
             SELECT i.{cfg.spatial_res}, d.{cfg.var}
             FROM index AS i
             LEFT JOIN (
-                SELECT {cfg.spatial_res}, {cfg.var} FROM read_parquet('{input_fname}')
-                {filter_condition}  -- Filtering before join
+                SELECT {cfg.spatial_res}, {time_col}, {cfg.var} FROM read_parquet('{input_fname}')
             ) AS d
-            ON i.{cfg.spatial_res} = d.{cfg.spatial_res}
+            {match_query}
         ) TO '{output_fname}' (FORMAT 'parquet');
     """
 
