bbaa27e5d2e8d58ef1859c6b0f27f56677e656c5
audiracmichelle@gmail.com <audiracmichelle@example.com>
2025-07-21 01:00:42 -0400

    fixed setup.py dependencies

--- setup.py
+++ setup.py
@@ -7,15 +7,13 @@ setup(
     install_requires=[
         'numpy',
         'torch',
-        'pandas==2.2.2',
-        'pyarrow==11.0.0',
-        'duckdb==0.9.2',
-        'hydra-core==1.3.2',
-        'snakemake==8.16',
-        'tqdm'
+        'pandas',
+        'pyarrow',
+        'duckdb',
+        'hydra-core',
     ],
     author='NSAPH Team',
     description='Data loader and preprocessor for Lego datasets',
     url='https://github.com/NSAPH-Data-Processing/LegoLoaderX',
-    python_requires='>=3.11',
+    python_requires='>=3.10',
 )
